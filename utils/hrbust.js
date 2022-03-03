5c2b8d679386173fbff96cf56a726a8738ad2dd5
smackgg <smackgg@gmail.com>
2022-03-03 14:53:50 +0800

    fix: fixed course bug

--- utils/hrbust.js
+++ utils/hrbust.js
@@ -16,6 +16,10 @@ const requestHeader = {
   'Accept-Encoding': 'gzip, deflate',
   Origin: 'http://jwzx.hrbust.edu.cn',
   'Content-Type': 'application/x-www-form-urlencoded',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Host': 'jwzx.hrbust.edu.cn',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+
 }
 // const getCaptcha = require('./captcha.js')
 // const mongoUtils = require('../../spider/mongoUtils')
