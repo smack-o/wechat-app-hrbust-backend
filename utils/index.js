7b5af327ea4b573eec911be443aa90c83e29e16f
smackgg <smackgg@gmail.com>
2023-11-28 10:44:03 +0800

    feat: check api add new params

--- utils/index.js
+++ utils/index.js
@@ -1,30 +1,30 @@
 // const debug = require('debug')('techmeme-backend:error');
 const { promisify } = require('util')
-const redis = require('redis')
-const redisClient = redis.createClient()
+const redisClient = require('./redis')
+
 const config = require('../config/config')
 
-redisClient.getAsync = (...args) => {
-  // return promisify(redisClient.get).bind(redisClient)
-  return promisify(redisClient.get).bind(redisClient)(...args).catch(err => {
-    console.error('get redis error: ', err)
-    return Promise.resolve()
-  }).then(res => {
-    console.log('get data from redis success');
-    return res;
-  })
-}
+// redisClient.getAsync = (...args) => {
+//   // return promisify(redisClient.get).bind(redisClient)
+//   return promisify(redisClient.get).bind(redisClient)(...args).catch(err => {
+//     console.error('get redis error: ', err)
+//     return Promise.resolve()
+//   }).then(res => {
+//     console.log('get data from redis success');
+//     return res;
+//   })
+// }
 
-redisClient.setAsync = (...args) => {
-  // return promisify(redisClient.get).bind(redisClient)
-  return promisify(redisClient.set).bind(redisClient)(...args).catch(err => {
-    console.error('set redis error: ', err)
-    return Promise.resolve()
-  }).then(res => {
-    console.log('set data to redis success');
-    return res;
-  })
-}
+// redisClient.setAsync = (...args) => {
+//   // return promisify(redisClient.get).bind(redisClient)
+//   return promisify(redisClient.set).bind(redisClient)(...args).catch(err => {
+//     console.error('set redis error: ', err)
+//     return Promise.resolve()
+//   }).then(res => {
+//     console.log('set data to redis success');
+//     return res;
+//   })
+// }
 
 
 const PRODUCTION = process.env.NODE_ENV === 'production'
